b4665fcb1471470d6fb21a7e67a4f6c069ad061f
TiagoRodriguesMattar <20100068@notredamecampinas.net.br>
2023-06-09 10:51:05 -0300

    TDD jest
    test driven development (TDD) usando jest sendo feito na função Objetovazio()

--- backend/main.js
+++ backend/main.js
@@ -770,7 +770,7 @@ app.post('/Show_Hist_Aluno', async (req,res) => {
     //console.log(result)
     obj2.Reprovados = result;
     console.log(obj2);
-    //res.json(obj2);
+    res.json(obj2);
   })
 })
 
