caf565a7ee63a1a8cb10a00179d48f5fb3a49307
Gabriel Garcia <gabrielsg.2604@gmail.com>
2023-06-19 19:00:35 -0300

    Update AlunoTreino.js
    correção tabela treinamento aluno

--- frontend/src/CrudTreinamentosJs/AlunoTreino.js
+++ frontend/src/CrudTreinamentosJs/AlunoTreino.js
@@ -28,17 +28,15 @@ function displayTreinamentos() {
         const obj = response.data;
         for(let i = 0; i < obj.length; i++) {
             const div = document.createElement("div");
-            div.innerHTML = `<table>
+            div.innerHTML = `<h2>Treinamento: ${obj[i].nome_comercial}</h2>
+            
+            <table>
             <thead>
                 <tr>
                     <th><b>Nome do treino</b></th>
                     <th><b>Código</b></th>
                     <th><b>Descrição</b></th>
                     <th><b>Carga Horária</b></th>
-                    <th><b>Início das inscrições</b></th>
-                    <th><b>Fim das inscrições</b></th>
-                    <th><b>Início dos treinamentos</b></th>
-                    <th><b>Fim dos treinamentos</b></th>
                 </tr>
             </thead>
     
@@ -48,13 +46,70 @@ function displayTreinamentos() {
                     <th>${obj[i].codigo}</th>
                     <th>${obj[i].descricao}</th>
                     <th>${obj[i].carga_horaria}</th>
-                    <th>${obj[i].inicio_inscricoes}</th>
-                    <th>${obj[i].fim_inscricoes}</th>
-                    <th>${obj[i].inicio_treinamento}</th>
-                    <th>${obj[i].fim_treinamento}</th>
                 </tr>
-
             <tbody>
+    
+            </table>
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th><b>Início das inscrições</b></th>
+                        <th><b>Fim das inscrições</b></th>
+                        <th><b>Início dos treinamentos</b></th>
+                        <th><b>Fim dos treinamentos</b></th>
+                    </tr>
+                </thead>
+    
+            <tbody>
+                    <tr>
+                        <th>${obj[i].inicio_inscricoes}</th>
+                        <th>${obj[i].fim_inscricoes}</th>
+                        <th>${obj[i].inicio_treinamento}</th>
+                        <th>${obj[i].fim_treinamento}</th>
+                    </tr>
+            <tbody>
+    
+            </table>
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th><b>Mínimo de inscritos</b></th>
+                        <th><b>Máximo de inscritos</b></th>
+                        <th><b>Nome Quiz de Aptidao</b></th>
+                        <th><b>Nome Case 1</b></th>
+                        <th><b>Nome Case 2</b></th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <tr>
+                        <th>${obj[i].min_inscritos}</th>
+                        <th>${obj[i].max_inscritos}</th>
+                        <th>${obj[i].QAptidao}</th>
+                        <th>${obj[i].QCase1}</th>
+                        <th>${obj[i].QCase2}</th>
+                    </tr>
+                <tbody>
+    
+            </table>
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th><b>Texto Curso 1</b></th>
+                        <th><b>Texto Curso 2</b></th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <tr>
+                        <th>${obj[i].Curso1}</th>
+                        <th>${obj[i].Curso2}</th>
+                    </tr>
+                <tbody>
+    
             </table>`;
             treinamentos.appendChild(div);
         }
